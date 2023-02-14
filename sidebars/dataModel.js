be8e141ec407dda1237d397a9b5ff9efba635494
Max Clayton Clowes <max@mcclowes.com>
2023-02-14 17:40:28 +0000

    Fixing links

--- sidebars/dataModel.js
+++ sidebars/dataModel.js
@@ -118,9 +118,9 @@ module.exports = [
     collapsed: true,
     items: [
       //"data-model/shared/datamodel-shared-country",
-      "/codat-api#/schemas/currency",
-      "/codat-api#/schemas/currencyrate",
-      "/codat-api#/schemas/datetime",
+      // "/codat-api#/schemas/currency",
+      // "/codat-api#/schemas/currencyrate",
+      // "/codat-api#/schemas/datetime",
       //"data-model/shared/valid-data-type-links",
     ],
   },
