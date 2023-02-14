b48689ff6bc1909bdc2e41e9d9c649085bdb0a5f
Max Clayton Clowes <max@mcclowes.com>
2023-02-14 17:52:34 +0000

    Update dataModel.js

--- sidebars/dataModel.js
+++ sidebars/dataModel.js
@@ -112,16 +112,16 @@ module.exports = [
     collapsed: true,
     items: ["data-model/bank-feeds/bank-feeds", ...paths.bankFeeds, ,],
   },
-  {
-    type: "category",
-    label: "Shared",
-    collapsed: true,
-    items: [
+//  {
+ //   type: "category",
+  //  label: "Shared",
+  //  collapsed: true,
+ //   items: [
       //"data-model/shared/datamodel-shared-country",
       // "/codat-api#/schemas/currency",
       // "/codat-api#/schemas/currencyrate",
       // "/codat-api#/schemas/datetime",
       //"data-model/shared/valid-data-type-links",
-    ],
-  },
+ //   ],
+//  },
 ];
