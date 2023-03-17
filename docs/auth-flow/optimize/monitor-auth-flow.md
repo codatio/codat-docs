adb4ed2d788fb3e71693c331590438c1886cf563
pzaichkina <112084241+pzaichkina@users.noreply.github.com>
2023-03-17 19:00:51 +0000

    Structure setup

---
+++ docs/auth-flow/optimize/monitor-auth-flow.md
@@ -0,0 +1,6 @@
+---
+title: "Monitor your auth flow"
+description: "Keep track of the customers' auth journey success rates across all integration types"
+---
+
+
--- sidebars/authFlow.js
+++ sidebars/authFlow.js
@@ -62,6 +62,7 @@ module.exports = [
     label: "Optimize your auth flow",
     collapsed: true,
     items: [
+      "auth-flow/optimize/monitor-auth-flow",
       "auth-flow/optimize/optimize-the-connection-journey",
       "auth-flow/optimize/funnel",
       "auth-flow/optimize/value-exchange",
