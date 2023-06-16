ffc98d45f1d71beaabfbf6f3167da133b5bcc3f7
Max Clayton Clowes <contact@mcclowes.com>
2023-06-16 13:19:54 +0100

    Funky product logos

--- src/components/global/Products/index.js
+++ src/components/global/Products/index.js
@@ -2,49 +2,56 @@ import React from "react";
 
 const allProducts = [
   {
-    logo: "/logos/products/Accounting.svg",
+    logo: "/logos/products-clear/accounting.svg",
+    slug: "accounting",
     name: "Accounting API",
     description: "Connect to your customers' accounts through our universal API.",
     link: "/accounting-api/overview",
     linkText: "See our accounting data and integration coverage →",
   },
   {
-    logo: "/logos/products/Banking.svg",
+    logo: "/logos/products-clear/Banking.svg",
+    slug: "banking",
     name: "Banking API",
     description: "Use Open Banking data for bank accounts and transactions alongside our other best-in-class products.",
     link: "/banking-api/overview",
     linkText: "See our banking data and integration coverage →",
   },
   {
-    logo: "/logos/products/Commerce.svg",
+    logo: "/logos/products-clear/Commerce.svg",
+    slug: "commerce",
     name: "Commerce API",
     description: "Connect to payment, Point of Sale, and eCommerce platforms and build with your customers' sales, orders, and payments data.",
     link: "/commerce-api/overview",
     linkText: "See our commerce data and integration coverage →",
   },
   {
-    logo: "/logos/products/BankFeeds.svg",
+    logo: "/logos/products-clear/BankFeeds.svg",
+    slug: "bank-feeds",
     name: "Bank Feeds API",
     description: "Enable your SMB users to set up bank feeds from accounts in your application to supported accounting platforms.",
     link: "/bank-feeds-api/overview",
     linkText: "Build your first bank feed →",
   },
   {
-    logo: "/logos/products/Assess.svg",
+    logo: "/logos/products-clear/Assess.svg",
+    slug: "assess",
     name: "Assess",
     description: "Make smarter credit decisions for your small business customers. Enrich your customer's financial data and receive actionable insights.",
     link: "/assess/get-started",
     linkText: "Start assessing your customers →",
   },
   {
-    logo: "/logos/products/SyncforExpenses.svg",
+    logo: "/logos/products-clear/SyncforExpenses.svg",
+    slug: "sync-for-expenses",
     name: "Sync for Expenses",
     description: "Embedded accounting integrations for corporate card providers. Standardize how you sync with bookkeeping software and ERPs.",
     link: "/sync-for-expenses/overview",
     linkText: "Read more",
   },
   {
-    logo: "/logos/products/SyncforCommerce.svg",
+    logo: "/logos/products-clear/SyncforCommerce.svg",
+    slug: "sync-for-commerce",
     name: "Sync for Commerce",
     description: "Sync your merchants' data between commerce and accounting systems, and automate bookkeeping.",
     link: "/sfc/overview",
@@ -69,7 +76,7 @@ const Products = ({mini, products, verbose}) => {
                     <a href={product.link}>
                       <img
                         src={product.logo}
-                        className="icon product"
+                        className="icon product hypercard"
                       />
                     </a>
                   </div>
@@ -98,7 +105,7 @@ const Products = ({mini, products, verbose}) => {
           return (
             <li ley={i} className="card">
               <div className="header">
-                <a href={product.link}>
+                <a href={product.link} className={`hypercard ${product.slug}`}>
                   <img
                     src={product.logo}
                     className="icon product"
