d7da152ce22d6d85415c7fb9b784ea5d8fdc34a3
Max Clayton Clowes <max@mcclowes.com>
2023-07-05 13:28:15 +0100

    Update src/components/global/Integrations/integrations.js

--- src/components/global/Integrations/integrations.js
+++ src/components/global/Integrations/integrations.js
@@ -40,7 +40,7 @@ export const sfeIntegrations = [
     "tags": ["Credit Cards only"],
     "img": "https://static.codat.io/public/officialLogos/Square/9815B998-024D-4415-A40B-89903B2CAD6F.png"
   },
-    {
+  {
     "name": "Dynamics 365 Business Central",
     "to": "/integrations/accounting/dynamics365businesscentral/accounting-dynamics365businesscentral",
     "type": "Sync For Expenses",
