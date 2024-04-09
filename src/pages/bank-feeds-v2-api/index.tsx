84964efd68b3f37209bd88c33de1cfb641c5d4d6
dcoplowe <dcoplowe@gmail.com>
2024-04-09 16:36:26 +0100

    Added Bank Feeds v2 API ref. (hidden)

---
+++ src/pages/bank-feeds-v2-api/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Api from '../../components/global/Api'
+
+const URL = "/oas/Codat-Bank-Feeds-V2.json"
+
+const BankFeedV2Api = () => {
+  return (
+    <Api url={URL} title="Bank Feeds (v2) API reference"/>
+  );
+}
+
+export default BankFeedV2Api
