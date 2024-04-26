0443ab9ba354d8a1f5aa608a7992f58db9024bb2
Max Clayton Clowes <contact@mcclowes.com>
2024-04-26 11:50:49 +0100

    Fix zapier imports

--- src/components/global/ZapierEmbed/index.js
+++ src/components/global/ZapierEmbed/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from './styles.module.scss';
 
-// Load JS
-import script from "https://cdn.zapier.com/packages/partner-sdk/v0/zapier-elements/zapier-elements.esm.js";
-//import "https://cdn.zapier.com/packages/partner-sdk/v0/zapier-elements/zapier-elements.css";
-
 const ZapierEmbed = () => {
   return (
     <div className={styles.zapierWrapper}>
+      <script type="module" src="https://cdn.zapier.com/packages/partner-sdk/v0/zapier-elements/zapier-elements.esm.js"/>
       <link rel="stylesheet" href="https://cdn.zapier.com/packages/partner-sdk/v0/zapier-elements/zapier-elements.css"/>
+
       <zapier-zap-templates
         client-id="bqKPWf4ag1p6YV7hAvRLDMBxzKUYJntEONiR3GIZ"
         theme="light"
