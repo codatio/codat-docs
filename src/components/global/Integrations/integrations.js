0e6c09e9271ec39f804fd890ad9e5d01f73902ed
Amy Roberts <125892487+Amy-Roberts@users.noreply.github.com>
2024-04-29 17:21:59 +0100

    Update integrations.js

--- src/components/global/Integrations/integrations.js
+++ src/components/global/Integrations/integrations.js
@@ -68,6 +68,7 @@ export const integrationsFilterSupplierEnablement = [
 
 export const integrationsFilterBillPayKit = [
   "Xero",
+  "QuickBooks Online",
 ];
 
 export const bankfeedsIntegrations = [
