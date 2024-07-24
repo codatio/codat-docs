16987deb84f10465ce4809edc8388754ccc3b5f1
Max Clayton Clowes <contact@mcclowes.com>
2024-07-24 10:16:38 +0800

    Fix social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -164,7 +164,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: '/img/meta/codat-bg.png',
 
       metadata: [
         { name: "keywords", content: "codat, docs, updates" },
