dc4fc8b7e95da6f0a44ced0f79f0ee4113430555
Max Clayton Clowes <contact@mcclowes.com>
2024-09-13 18:03:25 +0100

    Fix script

--- src/utils/oas-sitemap.js
+++ src/utils/oas-sitemap.js
@@ -61,14 +61,13 @@ const generateAPISitemaps = () => {
 	]
 
 
-	const sitemap = `
-		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-			${refs.map(ref => generateAPISitemap(ref[0], ref[1]))}
-		</urlset>
+	const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+	${refs.map(ref => generateAPISitemap(ref[0], ref[1]))}
+</urlset>
 		`
 
-	//console.log(sitemap)
+	console.log(sitemap)
 	return sitemap
 }
 
-generateAPISitemaps()
+return generateAPISitemaps()
