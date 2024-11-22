0598397d1ab2e1eb307da73c14f7a6dc5f2f3a18
Max Clayton Clowes <contact@mcclowes.com>
2024-11-22 15:54:37 +0000

    Delete lottie

--- src/components/Lottie/index.js
+++ src/components/Lottie/index.js
@@ -1,29 +1,32 @@
-import React, { Suspense } from "react";
-import Lottie from "lottie-react";
-
+import React, { Suspense, lazy } from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
 import styles from "./styles.module.scss";
 
+// Dynamically import the Lottie component
+const Lottie = lazy(() => import("lottie-react"));
+
 const App = (props) => {
 	const {
 		animation,
-		loop=true,
+		loop = true,
 		...rest
-	} = props
+	} = props;
 
-return <BrowserOnly>
-	{ 
-		() => <Suspense>
-			<Lottie
-				className={styles.wrapper}
-				animationData={animation}
-				loop={loop}
-				{...rest}
-			/>
-		</Suspense>
-	}
-</BrowserOnly>
+	return (
+		<BrowserOnly>
+			{() => (
+				<Suspense fallback={<div>Loading...</div>}>
+					<Lottie
+						className={styles.wrapper}
+						animationData={animation}
+						loop={loop}
+						{...rest}
+					/>
+				</Suspense>
+			)}
+		</BrowserOnly>
+	);
 };
 
 export default App;
