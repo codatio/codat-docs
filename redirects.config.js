97a23d0aaee8922933bd18036bbf03154ff8f061
Polina <112084241+pzaichkina@users.noreply.github.com>
2025-01-13 13:06:17 +0000

    delete legacy quickbooks docs

--- redirects.config.js
+++ redirects.config.js
@@ -457,10 +457,7 @@ module.exports = {
   }, {
     from: "/integrations/commerce/commerce-platform-keys",
     to: "/integrations/commerce/overview"
-  }, {
-    from: "/integrations/accounting/quickbooksdesktop/install-qbd-connector-right-networks",
-    to: "/integrations/accounting/quickbooksdesktop/legacy/install-qbd-connector-right-networks"
-  },
+  }, 
   // sfc
 
   {
