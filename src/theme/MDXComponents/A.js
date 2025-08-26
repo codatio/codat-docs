34870f25042d42b06e1ce437857d17ddaebade44
pmckinney-codat <p.mckinney@codat.io>
2025-08-26 12:49:13 +0100

    feat[PEP-379]: Add Amplitude Tracking

--- src/theme/MDXComponents/A.js
+++ src/theme/MDXComponents/A.js
@@ -1,5 +1,29 @@
 import React from "react";
 import Link from "@docusaurus/Link";
+import { trackLinkClick } from "../../utils/amplitude";
+
 export default function MDXA(props) {
-  return <Link {...props} />;
+  const handleClick = () => {
+    if (props.href) {
+      const isExternal =
+        props.href.startsWith("http") || props.href.startsWith("//");
+      const isDownload =
+        props.href.includes("download") ||
+        props.href.endsWith(".pdf") ||
+        props.href.endsWith(".zip") ||
+        props.href.endsWith(".doc") ||
+        props.href.endsWith(".docx") ||
+        props.href.endsWith(".xlsx");
+
+      let linkType = "internal";
+      if (isExternal) linkType = "external";
+      if (isDownload) linkType = "download";
+
+      trackLinkClick(props.href, props.children || props.title, linkType, {
+        component: "mdx_link",
+      });
+    }
+  };
+
+  return <Link {...props} onClick={handleClick} />;
 }
--- src/theme/NavbarItem/index.tsx
+++ src/theme/NavbarItem/index.tsx
@@ -1,10 +1,11 @@
 import OriginalNavbarItem from "@theme-original/NavbarItem";
 import React from "react";
-import NavbarIconLink from "@theme/NavbarItem/NavbarIconLink";
-import NavbarSeparator from "@theme/NavbarItem/NavbarSeparator";
-import NavbarCta from "@theme/NavbarItem/NavbarCta";
+import NavbarIconLink from "./NavbarIconLink";
+import NavbarSeparator from "./NavbarSeparator";
+import NavbarCta from "./NavbarCta";
 
 import { track } from "@vercel/analytics";
+import { trackEvent } from "../../utils/amplitude";
 
 const CustomNavbarItemComponents = {
   iconLink: () => NavbarIconLink,
@@ -18,6 +19,16 @@ export default function NavbarItem(props) {
   const onTrack = () => {
     if (props.label === "Sign in") {
       track("Sign in");
+      trackEvent("Navigation Click", {
+        element: "Sign in",
+        type: "navbar_cta",
+      });
+    } else if (props.label) {
+      trackEvent("Navigation Click", {
+        element: props.label,
+        type: "navbar_item",
+        href: props.href || props.to,
+      });
     }
   };
 
